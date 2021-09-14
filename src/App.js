44566cdc9781d0f1631f12a8d13386fdc58138e8
Your Name <csyedabdulrafeyali@gmail.com>
2021-09-15 04:27:20 +0500

    main__frontend__start

--- src/App.js
+++ src/App.js
@@ -2,30 +2,43 @@ import './App.css';
 import SearchBar from './SearchBar';
 import Slider from './Slider';
 
+import one from './images/1.svg';
 const slideData = [
   {
     index: 0,
-    headline: 'New Fashion Apparel',
-    button: 'Shop now',
-    src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/fashion.jpg'
+    headline: 'Hashtag Initiators',
+    button: '@ImranKhan PTI',
+    src: "https://images.pexels.com/photos/9558678/pexels-photo-9558678.png"
   },
   {
     index: 1,
-    headline: 'In The Wilderness',
-    button: 'Book travel',
-    src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/forest.jpg'
+    headline: 'Total Users Tweeting',
+    button: '@1185',
+    src: 'https://images.pexels.com/photos/9558679/pexels-photo-9558679.png'
   },
   {
     index: 2,
-    headline: 'For Your Current Mood',
-    button: 'Listen',
-    src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/guitar.jpg'
+    headline: 'Total Tweets',
+    button: '@20000',
+    src: 'https://images.pexels.com/photos/9558680/pexels-photo-9558680.png'
   },
   {
     index: 3,
-    headline: 'Focus On The Writing',
-    button: 'Get Focused',
-    src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/typewriter.jpg'
+    headline: 'Retweets',
+    button: '@1359',
+    src: 'https://images.pexels.com/photos/9558681/pexels-photo-9558681.png'
+  },
+  {
+    index: 4,
+    headline: 'Likes',
+    button: '@874',
+    src: 'https://images.pexels.com/photos/9558682/pexels-photo-9558682.png'
+  },
+  {
+    index: 5,
+    headline: 'Person with highest tweets',
+    button: '@Zulfiqar',
+    src: 'https://images.pexels.com/photos/9558683/pexels-photo-9558683.png'
   }
 ]
 
