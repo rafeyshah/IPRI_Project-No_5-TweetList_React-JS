d9c566c66d9750e5bca78737e60764b2e53e0c03
Your Name <csyedabdulrafeyali@gmail.com>
2021-09-15 03:36:50 +0500

    main__slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
 import SearchBar from './SearchBar';
+import Slider from './Slider';
+
+const slideData = [
+  {
+    index: 0,
+    headline: 'New Fashion Apparel',
+    button: 'Shop now',
+    src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/fashion.jpg'
+  },
+  {
+    index: 1,
+    headline: 'In The Wilderness',
+    button: 'Book travel',
+    src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/forest.jpg'
+  },
+  {
+    index: 2,
+    headline: 'For Your Current Mood',
+    button: 'Listen',
+    src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/guitar.jpg'
+  },
+  {
+    index: 3,
+    headline: 'Focus On The Writing',
+    button: 'Get Focused',
+    src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/typewriter.jpg'
+  }
+]
 
 function App() {
   return (
-    <SearchBar />
+    <>
+      <SearchBar />
+      <Slider heading="Example Slider" slides={slideData} />
+    </>
   );
 }
 
---
+++ src/Item.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Item({ img }) {
+    return (
+        < >
+            <img style={{ height: 53, width: 36 }} src={img} alt="" />
+        </>
+    )
+}
+
+export default Item
